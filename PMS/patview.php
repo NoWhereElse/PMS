edd3fac361b3dd850abe7551f3bfc26235f27f24
Amit Hublikar <nowhereelse@googlegroups.com>
2014-01-05 18:52:03 +0530

    new

--- PMS/patview.php
+++ PMS/patview.php
@@ -1,6 +1,39 @@
 <?php
 include ("./Admin.php");
 ?>
+<?php
+#$pname=$_POST["pname"];
+$pname= (isset($_POST["pname"]) ? $_POST["pname"] : ""); 
+
+
+#echo "$uname";
+#echo "$pass";
+
+$username = "root";
+$password = "";
+$hostname = "localhost"; 
+
+
+$dbhandle = mysql_connect($hostname, $username, $password)
+  or die("Unable to connect to MySQL");
+#echo "Connected to MySQL<br>";
+  
+//select a database to work with
+$selected = mysql_select_db("pms",$dbhandle)   or die("Could not select pms");
+  
+if($pname===NULL)
+    {
+	$pname ="%";
+	}
+  
+  $result = mysql_query("select PID,FName,LName,DOB from patients where PID='" . $pname . "' and Fname='" . $pname . "'");
+  
+   if (!$result) {
+die("Database query failed: " . mysql_error());
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -25,21 +58,43 @@ $(function() {
 		$( "#datepicker" ).datepicker();
 	});
 </script>
+<script language="javascript">
+function search(a)
+{
+alert(a);
+document.PMS.action="patview.php?env="+a;
+document.PMS.submit();
+}
+</script>
 
 
 </head>
 <body bgcolor="silver">
-<form action="patinsert.php" method="post">
+<form  name="PMS" method="post">
 <div style="position:absolute; left:400px; top:110px;">   
 <table class="myTable" border='2'; bgcolor='white' ;border cellpadding=15; width='800'> 
+<legend>
+<fieldset>Patient Details</fieldset>
 <tr>
 <td>Pat ID/Name:</td>
-<td ><input type=text name=pname value="" size="25"></td><td> <input type="submit" value="SEARCH" onClick="search();" > </td>
+<td ><input type=text name=pname value="" size="25"></td><td> <input type="submit" value="SEARCH" onClick="search(Search.value);"></td>
+
 </tr>
 <tr>
-<th>Pid</th><th>Name</th><th>Age</th>
-</tr>
 
+<th>Pid</th><th>FName</th><th>LName</th><th>Age</th>
+<?php
+while($row = mysql_fetch_array($result))
+  {
+  echo"<tr>";
+  echo "<td >" . $row['PID'] . "</td>";
+  echo "<td>" . $row['FName'] . "</td>";
+  echo "<td>" . $row['LName'] . "</td>";
+  echo "<td>" . $row['DOB'] . "</td>";
+echo "</tr>";
+  }
+
+?>
 </table>
 </div>
 </body>
